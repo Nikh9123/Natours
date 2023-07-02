09569975eb6ef6d8d0fe19c8857f3eca7c45d392
Nikh9123 <satyamnkhil1524@gmail.com>
2023-07-02 19:14:21 +0530

    starting new section error handling

--- app.js
+++ app.js
@@ -8,23 +8,17 @@ const tourRouter = require('./routes/tourRoutes');
 const app = express();
 
 //todo: <1> MIDDLEWARES
-if(process.env.NODE_ENV==='development')
-{
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
-  // console.log(morgan('dev')) 
+  // console.log(morgan('dev'))
 }
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
-
-app.use((req, res, next) => {
-  console.log('hello from middleware 👍');
-  next();
-});
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
-}); 
+});
 
 //todo: <2> ROUTE HANDLERS
 
@@ -34,11 +28,33 @@ app.use((req, res, next) => {
 
 //todo :<5> USERS ROUTE
 
+//Mounting
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message:`No such ${req.originalUrl} route exists`,
+  // });
+  const err = new Error(
+    `No such ${req.originalUrl} route exists on this server`
+  );
+  err.statusCode = 404;
+  err.status = `fail`;
 
+  next(err);
+});
 
-//Mounting 
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/tours' , tourRouter)
+//^ global error handling
+app.use((err, req, res, next) => {
+  // console.log("😍😍😍😍",err.stack);
+  err.statusCode = err.statusCode || 500; //500 means internal server error
+  err.status = err.status || 'error';
 
+  res.status(err.statusCode).json({
+    status : err.status ,
+    message: err.message 
+  });
+});
 //todo: <6> START SERVER
-module.exports = app ;
+module.exports = app;
